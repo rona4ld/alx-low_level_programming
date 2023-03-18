8b543bdb1a1cf8e59ee7fd85a3e9b84f1bff81cd
rona4ld <ronaldchirchir5634@gmail.com>
2023-03-18 01:55:23 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 /**
- * Descripition : main - prints out a string to stdout.
- * Returns: 0 if success.
+ * main - Entry point
+ *
+ *  Return: Always 0 (Success)
  */
 int main(void)
 {
