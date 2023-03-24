374da4cbe7605761ec9f37166e3ffa1bace24790
rona4ld <ronaldchirchir5634@gmail.com>
2023-03-24 07:27:04 -0700

    leap year

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -8,7 +8,7 @@
  * @year: year
  * Return: void
  */
-void print_remainingdays(int month, int day, int year)
+void print_remaining_days(int month, int day, int year)
 {
         if (year % 4 == 0 || ((year % 100 == 0) && (year % 400 == 0)))
         {
