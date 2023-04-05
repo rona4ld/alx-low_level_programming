cf0f6fa05cea4404a1a7d9ae9deb3265c25d4834
rona4ld <ronaldchirchir5634@gmail.com>
2023-04-05 10:39:20 -0700

    4. Once an idea has taken hold of the brain it's almost impossible to eradicate

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -8,10 +8,8 @@ int _strlen_recursion(char *s)
 {
 	if (*s)
 	{
-		return (1 + _strlen_recursion(s + 1));
+		longit++;
+		longit += _strlen_recursion(s + 1);
 	}
-	else
-	{
-		return (0);
-	}
+	return (longit);
 }
