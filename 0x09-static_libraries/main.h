ded37eae309ccec063d1ec7e3ae9f7398bacd27c
rona4ld <ronaldchirchir5634@gmail.com>
2023-04-09 10:45:41 -0700

    prototypes

--- 0x09-static_libraries/main.h
+++ 0x09-static_libraries/main.h
@@ -1,5 +1,5 @@
-#ifndef MAIN_H
-#define MIAN_H
+#ifndef MY_HEADER_FILE_H
+#define MY_HEADER_FILE_H
 int _putchar(char c);
 int _islower(int c);
 int _isalpha(int c);
@@ -20,4 +20,4 @@ char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
-#endif
+#endif /*MY_HEADER_FILE_H*/
