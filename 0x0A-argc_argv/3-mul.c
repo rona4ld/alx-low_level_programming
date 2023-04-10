72c62d0f8ca5d1736c87fc7de281656737926bcd
rona4ld <ronaldchirchir5634@gmail.com>
2023-04-10 02:16:09 -0700

    my readme file

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -43,21 +43,22 @@ int _atoi(char *s)
  * main - multiply two numbers
  * @argc: number of arguments
  * @argv: array of arguments
- * Return : 0 success, 1 error
+ * Return : 0 on success, 1 on error
  */
 int main(int argc, char *argv[])
 {
 	int result, num1, num2;
 
-	if (argc < 3 || argc > 3)
+	if (argc < 3 || argc > 2)
 	{
 		printf("error\n");
 		return (1);
 	}
+
 	num1 = _atoi(argv[1]);
 	num2 = _atoi(argv[2]);
 	result = num1 * num2;
-
+}
 	printf("%d\n", result);
 
 	return (0);
