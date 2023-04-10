ff2e02890c918c8530fe787bf97deafa029f9ef0
rona4ld <ronaldchirchir5634@gmail.com>
2023-04-10 03:44:42 -0700

    3. Neither irony nor sarcasm is argument

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,70 +1,26 @@
 #include <stdio.h>
-#include "main.h"
+#include <stdlib.h>
 
 /**
- * _atoi - converts a string to an integer
- * @s: string to be converted
- * Return: the int converted from the string
+ * main - multiplies two numbers.
+ * @argc: argument count
+ * @argv: arguments
+ *
+ * Return: 0
  */
-int _atoi(char *s)
+int main(int argc, char **argv)
 {
-	int i, d, n, len, f, digit;
+	int x, y;
 
-	i = 0;
-	d = 0;
-	n = 0;
-	len = 0;
-	f = 0;
-	digit = 0;
-
-	while (s[len] != '\0')
-		len++;
-
-	while (i < len && f == 0)
+	if (argc < 3)
 	{
-		if (s[i] == '-')
-			++d;
-		if (s[i] >= '0' && s[i] <= '9')
-		{
-			digit = s[i] - '0';
-			if (d % 2)
-				digit = -digit;
-			n = n * 10 + digit;
-			f = 1;
-			if (s[i + 1] < '0' || s[i + 1] > '9')
-				break;
-			f = 0;
-		}
-		i++;
+		printf("Error\n");
+		return (1);
 	}
 
-	if (f == 0)
-		return (0);
+	x = atoi(argv[1]);
+	y = atoi(argv[2]);
+	printf("%d\n", x * y);
 
-	return (n);
-}
-
-/**
- * main - multiply two numbers
- * @argc: number of arguments
- * @argv: array of arguments
- * Return: 0 on success, 1 on error
- */
-int main(int argc, char *argv[])
-{
-	int result, num1, num2;
-
-	if (argc != 3)
-	{
-		printf("Error: The program requires two arguments\n");
-		return (1); /* indicates an error occurred*/
-	}
-
-	num1 = _atoi(argv[1]);
-	num2 = _atoi(argv[2]);
-	result = num1 * num2;
-
-	printf("%d\n", result);
-
-	return (0); /* indicates successful program execution*/
+	return (0);
 }
