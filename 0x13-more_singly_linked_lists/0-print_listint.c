b620cece3f58b0fddc03f828f82e347df1435b0a
rona4ld <ronaldchirchir5634@gmail.com>
2023-05-01 12:02:48 -0700

    my correction

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -4,12 +4,13 @@
  * @h: linked list of type listint_t to print
  * Return: number of nodes
  */
-size _t print_listint(const listint_t *h)
+size_t print_listint(const listint_t *h)
 {
-	size_t num +0;
+	size_t num = 0;
+
 	while (h)
 	{
-		print("%d\n", h->n);
+		printf("%d\n", h->n);
 		num++;
 		h = h->next;
 	}
